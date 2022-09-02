e6aeaf02198b92a000552715fd80d224181a9a5d
SteevesSaillant <steevessaillant@gmail.com>
2022-09-02 11:43:10 -0400

    FIX 48,49

--- cypress/integration/common/login.js
+++ cypress/integration/common/login.js
@@ -3,20 +3,17 @@ import LoginPage from '../../pages/login-page';
 import LoginResultsPage from '../../pages/login-results-page';
 
 Given(/^browser is openened to the login page$/, () => {
-  throw new Error("Pending implementation");
-  //LoginPage.visit();
+   LoginPage.visit();
 });
 
 When('user {string} logs in with password {string}', (username, password) => {
-  throw new Error("Pending implementation");
-  //LoginPage.enter_username(username);
-  //LoginPage.enter_password(password);
-  //LoginPage.pressLogin();
+  LoginPage.enter_username(username);
+  LoginPage.enter_password(password);
+  LoginPage.pressLogin();
 });
 
 Then(/^welcome page should be opened$/, () => {
-    throw new Error("Pending implementation");
-    //LoginResultsPage.expect().toBeSuccessful();
+    LoginResultsPage.expect().toBeSuccessful();
   });
 
 Then(/^error page should be open$/, () => {
